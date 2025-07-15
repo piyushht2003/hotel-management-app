0f3377630b41c81fc6361e17c69aa4668c131533
Piyush <piyushsinghthakur128@gmail.com>
2025-07-15 22:54:03 +0530

    fixed

--- server/models/User.js
+++ server/models/User.js
@@ -4,10 +4,10 @@ const userSchema = mongoose.Schema({
     _id: {type: String, require: true},
     username: {type: String, require: true},
     email: {type: String, require: true},
-    email: {type: String, require: true},
+    image: {type: String, require: true},
     role: {type: String, enum: ["user", "hotelOwner"], default: "user"},
     recentSearchedCities: [{type: String, required: true}],
-},{timestamp: true});
+},{timestamps: true});
 
 const User = mongoose.model("User", userSchema);
 
