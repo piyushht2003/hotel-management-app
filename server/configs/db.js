b3f72184f64605784fd45e850b0cf95ee3670841
Piyush <piyushsinghthakur128@gmail.com>
2025-07-17 19:39:21 +0530

    file

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        mongoose.connection.on('connected', ()=>console.log("Databse connected")
-        );
+        mongoose.connection.on('connected', ()=>console.log("Databse connected"));
         await mongoose.connect(`${process.env.MONGODB_URI}`)
     }catch (error){
         console.log(error.message);
